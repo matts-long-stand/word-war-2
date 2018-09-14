16d4c91f29909d2a61829e884fc396f9452a4335
mmicciolo <mmicciolo@wpi.edu>
2018-09-14 12:20:21 -0400

    Power Up + Player Spawning.

---
+++ Word War II/Assets/Spawn/SpawnLocation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLocation : MonoBehaviour, ISpawnLocation {
+
+	// Use this for initialization
+	public virtual void Start () {
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		
+	}
+}
